f6ace0e6c4a4bf9b85194734dae2cff7dc1cdbf2
Louis Alridge <loualrid@gmail.com>
2018-06-15 12:43:40 -0700

    Switch Logo to times new roman, fix spacing

--- client/src/components/HeaderLogo/index.js
+++ client/src/components/HeaderLogo/index.js
@@ -4,17 +4,17 @@ import './style.css'
 export const HeaderLogo = props => {
   return (
     <div style={{fontSize: '30px'}}>
-      <div className='header-line-1 header-line' data-title='TheLocus'>
-        <h1 className='header-css'>TheLocus</h1>
+      <div className='header-line-1 header-line' data-title='THELOCUS'>
+        <h1 className='header-css'>THELOCUS</h1>
       </div>
-      <div className='header-line-2 header-line' data-title='Louis'>
-        <h1 className='header-css'>Louis</h1>
+      <div className='header-line-2 header-line' data-title='__________'>
+        <h1 className='header-css-divider'>__________</h1>
       </div>
-      <div className='header-line-3 header-line' data-title='______'>
-        <h1 className='header-css-divider header-line-3'>______</h1>
+      <div className='header-line-3 header-line' data-title='LOUIS'>
+        <h1 className='header-css'>LOUIS</h1>
       </div>
-      <div className='header-line-4 header-line' data-title='Alridge'>
-        <h1 className='header-css header-line-4'>Alridge</h1>
+      <div className='header-line-4 header-line' data-title='ALRIDGE'>
+        <h1 className='header-css header-line-4'>ALRIDGE</h1>
       </div>
     </div>
   )
